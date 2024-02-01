22b1a93e0d2f6567339bab603ea9cd0665a92f2d
mohand <mohanadalnjjar23@gmail.com>
2024-02-01 17:00:12 +0200

    generate hive type adapter

--- lib/features/to_do_feature/data/models/to_do_model.dart
+++ lib/features/to_do_feature/data/models/to_do_model.dart
@@ -1,5 +1,11 @@
-class ToDoModel {
-  final String ToDo;
+import 'package:hive/hive.dart';
 
-  ToDoModel({required this.ToDo});
+part 'to_do_model.g.dart';
+
+@HiveType(typeId: 0)
+class ToDoModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+
+  ToDoModel({required this.title});
 }
